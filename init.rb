300656fc3d81b78394fca208d830abf128f96b34
Mauricio Santos <maosan132@gmail.com>
2020-05-04 16:44:02 -0500

    finished with my_count

--- init.rb
+++ init.rb
@@ -30,7 +30,7 @@ module Enumerable
     self.my_each do |i| 
       #check = yield(i)
       #if yield i 
-      if yield(i) then ary << i 
+      if yield(i) then ary << i
       end
     
     end
@@ -66,9 +66,12 @@ module Enumerable
       self.my_each do |i|
         if yield(i) then count += 1
         end
+        return true if count == 1
       end
-      puts count > 0 ? true : false
+      # puts count > 0 ? true : false
+      return false
     end
+
 
   def my_none?
     count = 0
@@ -79,37 +82,48 @@ module Enumerable
       puts count == 0 ? true : false
     end
 
-    
+ 
 
+  def my_count(arg = nil)
+    count = 0
+    count = size if !block_given? && arg.nil?
+    self.my_each {|i| count += 1 unless i != arg} if !arg.nil? 
+    self.my_each {|i| count += 1 if yield(i)} if block_given?
+    puts count
+  end
+  
   def my_map
     ary = []
     self.my_each {|i| }
-
-
-
+  end
 
   #--------------------------------
-end # end module enum
+end
+# end module enum
 
-my_array = [1, 3, 4, 6, 78, 9]
+my_array = [1, 3, 4, 6, 78, 9, 3]
 my_range = (1..10)
 my_hash = { one: 'one', two: 'two', three: 'three' }
 my_strings = %w(Morris David Cris Stella)
 fruits_in_drawer = %w[lemon, apple, lemon, apple, grape, grape, pear, peach, kiwi, banana, banana]
 
 
-#my_array.my_each {|i| puts i }
-#my_array.my_each
-#my_array.my_each_with_index {|k,i| puts "#{k}: #{i}" }
+# my_array.my_each {|i| puts i }
+# my_array.my_each
+# my_array.my_each_with_index {|k,i| puts "#{k}: #{i}" }
 
 # my_range.my_each { |x| puts x }
 
-#my_strings.select { |x|  x.size < 5 ? }
-#uits_in_drawer.my_select_without_myeach {|i| i == "lemon"}
-#my_array.my_select {|i| i > 5}
-my_array.my_any? {|i| i == 5}
+# my_strings.select { |x|  x.size < 5 ? }
+# fruits_in_drawer.my_select_without_myeach {|i| i == "lemon"}
+# my_array.my_select {|i| i > 5}
+# my_array.my_any? {|i| i == 5}
+
+my_array.my_count()
+my_array.my_count(3)
+my_array.my_count {|i| i > 5}
 puts "-*-*-*-*-*--*-"
-my_array.my_all? {|i| i == 0}
+# my_array.my_all? {|i| i == 0}
 puts
 
 # my_strings.my_select { |x|  x.even? }
