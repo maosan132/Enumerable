ddde9f1caa20de7fc02c8cf16b4083c5af9e58c6
Mauricio Santos <maosan132@gmail.com>
2020-05-06 12:23:46 -0500

    created new file to contain enumerable module, and let init.rb as the caller file

--- init.rb
+++ init.rb
@@ -6,6 +6,7 @@ module Enumerable
       yield(i)
     end
     self
+    puts
   end
 
   def my_each_with_index
@@ -113,17 +114,13 @@ my_array = [1, 3, 4, 6, 78, 9, 3]
 my_range = (1..10)
 my_hash = { one: 'one', two: 'two', three: 'three' }
 my_strings = %w(Morris David Cris Stella)
-fruits_in_drawer = %w[lemon, apple, lemon, apple, grape, grape, pear, peach, kiwi, banana, banana]
 
 
-my_array.my_each { |i| puts i }
-my_array.my_each
-# my_array.my_each_with_index { |k,i| puts "#{k}: #{i}" }
-
-# my_range.my_each { |x| puts x }
+# my_array.my_each { |i| puts i }
+my_array.my_each_with_index { |k,i| puts "#{k}: #{i}" }
 
 # my_strings.select { |x|  x.size < 5 ? }
-# fruits_in_drawer.my_select_without_myeach { |i| i == "lemon"}
+# my_strings.my_select_without_myeach { |i| i == "lemon"}
 # my_array.my_select { |i| i > 5}
 # my_array.my_any? { |i| i == 5}
 
@@ -132,11 +129,12 @@ my_array.my_each
 # my_array.my_count { |i| i > 5}
 # my_array.my_inject6(2, :+)
 # my_array.my_inject6(:+)
-my_array.my_inject(2) { |m,i| m + i}
-my_array.my_inject { |m,i| m + i}
-
-multiply_els(my_array)
+ 
 #my_array.my_map { |i| i * 2}
 # my_array.my_all? { |i| i == 0}
 
 # my_strings.my_select { |x|  x.even? }
+
+puts "normal enumerables"
+my_array.each_with_index { |k,i| puts "#{k}: #{i}" }
+
